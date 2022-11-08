5a4114cd6b859255eba3ad7f122bea13a7567e72
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-08 12:26:12 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -8,6 +9,11 @@ const port = process.env.PORT || 5000;
 // middleware 
 app.use(cors());
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6llxg7j.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/',(req,res)=>{
     res.send('Nafis Photography Server is Running...')
